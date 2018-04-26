827589e63c4519bf881c175e67d9d9f575b07752
blankskygithub <blankskyg@gmail.com>
2018-04-26 17:54:55 +0800

    Add Column Transposition

--- Cryptographyunittest.py
+++ Cryptographyunittest.py
@@ -26,17 +26,45 @@ class TestEncryptModule(unittest.TestCase):
             raise e
         self.assertEqual('Tsrshaesistasemgicee', ciphertext)
 
+    def testColumnTransposition(self):
+        """
+                Test Column Transposition
+        """
+        plaintext = "this is a secret message"
+        key = [4, 5, [2, 3, 5, 1, 4]]
+        ciphertext = ""
+        try:
+            ciphertext = encrypt.column_transposition(plaintext, key)
+        except Exception as e:
+            print(e.message)
+            raise e
+        self.assertEqual('sthiiesacsmreetgssea', ciphertext)
+
 
 class TestDecryptModule(unittest.TestCase):
 
     def testRowColumnTransposition(self):
         """
-        Test Row ColumnTransposition
+        Test Row/ColumnTransposition
         """
         ciphertext = "Tsrshaesistasemgicee"
         key = [4, 5]
         self.assertEqual('Thisisasecretmessage',
                          decrypt.row_column_transposition(ciphertext, key))
+
+    def testColumnTransposition(self):
+        """
+        Test Column Transposition
+        """
+        ciphertext = "sthiiesacsmreetgssea"
+        key = [4, 5, [2, 3, 5, 1, 4]]
+        plaintext = ""
+        try:
+            plaintext = decrypt.column_transposition(ciphertext, key)
+        except Exception as e:
+            print e.message
+            raise e
+        self.assertEqual('thisisasecretmessage', plaintext)
 
 
 if __name__ == '__main__':
--- decrypt.py
+++ decrypt.py
@@ -26,15 +26,46 @@ def row_column_transposition(ciphertext, key):
         plaintext="Thisisasecretmessage"
     """
     plaintext = ''
-    row = key[1]
-    column = key[0]
-    if row * column != len(ciphertext):
+    num_rows = key[1]
+    num_columns = key[0]
+    if num_rows * num_columns != len(ciphertext):
         raise ce.EncryptKeyErrorException(
             "encrypt key error: num_row * num_column != ciphertext.__len__")
-    for i in xrange(0, column):
-        for j in xrange(0, row):
-            if (i + j * column) < len(ciphertext):
-                plaintext += ciphertext[i + j * column]
+    for i in xrange(0, num_columns):
+        for j in xrange(0, num_rows):
+            if (i + j * num_columns) < len(ciphertext):
+                plaintext += ciphertext[i + j * num_columns]
             else:
                 plaintext += ''
+    return plaintext
+
+
+def column_transposition(ciphertext, key):
+    """
+    Description: Column Transposition
+    Arguments:
+        ciphertext: cipher text
+        key(plaintext -> ciphertext): [num_rows, num_columns, [columns_transposition]]
+    Example:
+        ciphertext = "sthiiesacsmreetgssea"
+        key=[4, 5, [2, 3, 5, 1, 4]]
+        2 3 5 1 4
+        s t h i i
+        e s a c s
+        m r e e t
+        g s s e a
+        plaintext="thisisasecretmessage"
+    """
+    plaintext = ''
+    num_rows = key[0]
+    num_columns = key[1]
+    columns_position = key[2]
+    if (num_rows * num_columns != len(ciphertext)) or (num_columns != len(columns_position)):
+        raise ce.EncryptKeyErrorException(
+            "encrypt key error: num_rows * num_columns != plaintext.__len__(without spaces) \
+            OR num_columns != columns_position.__len__")
+    for i in xrange(0, num_rows):
+        for j in xrange(0, num_columns):
+            index = i * num_columns + columns_position[j] - 1
+            plaintext += ciphertext[index]
     return plaintext
